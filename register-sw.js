fd96687ac16faa7ae03b6d3abc10383afe1f3c9e
devpromptwc <suportedevprompt@gmail.com>
2025-06-24 21:56:45 -0400

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,9 +1,13 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-    .then(registration => {
-      console.log('Service Worker registrado com sucesso:', registration);
-    })
-    .catch(error => {
-      console.error('Falha ao registrar Service Worker:', error);
-    });
+  navigator.serviceWorker.register('https://devpromptwc.github.io/sw.js').then(function(registration) {
+    console.log('Service Worker registrado com sucesso:', registration);
+  }).catch(function(error) {
+    console.log('Falha ao registrar o Service Worker:', error);
+  });
 }
+
+// Adiciona o manifesto ao documento
+let link = document.createElement('link');
+link.rel = 'manifest';
+link.href = 'https://devpromptwc.io/manifest.json';
+document.head.appendChild(link);
