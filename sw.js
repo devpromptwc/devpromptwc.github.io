7de471a179b2527c728d5383d59ecb96e1d3ce89
devpromptwc <suportedevprompt@gmail.com>
2025-06-25 00:41:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,16 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('app-devprompt-cache').then(function(cache) {
-      return cache.addAll([
-        'https://devprompt.blogspot.com/',
-        'https://devpromptwc.github.io/sw.js',
-        'https://devpromptwc.github.io/manifest.json',
-        'https://devpromptwc.github.io/icons/icon192.png',
-        'https://devpromptwc.github.io/icons/icon512.png'
-      ]);
-    })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app-cache').then(cache =>
+      cache.addAll([
+        '/',
+        '/index.html'
+      ])
+    )
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
