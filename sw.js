6a9c77560815901e855c3de3fbc4a4f7ae034f5f
devpromptwc <suportedevprompt@gmail.com>
2025-06-24 21:54:33 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,11 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('app-devprompt-cache').then(function(cache) {
       return cache.addAll([
-        'https://devprompt.blogspot.com//',
-        'https://devprompt.blogspot.com/sw.js',
-        'https://devprompt.blogspot.com/manifest.json',
-        'https://devprompt.blogspot.com/icons/icon-192x192.png',
-        'https://devprompt.blogspot.com/icons/icon-512x512.png'
+        'https://devprompt.blogspot.com/',
+        'https://devpromptwc.github.io/sw.js',
+        'https://devpromptwc.github.io/manifest.json',
+        'https://devpromptwc.github.io/icons/icon192.png',
+        'https://devpromptwc.github.io/icons/icon512.png'
       ]);
     })
   );
